808d0f374b20596940991922b3ebf41fb1ef76a7
Jamie Ingalls <jamesmaclay@gmail.com>
2024-04-06 13:05:11 -0400

    Created Book component

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    title: "Dune",
+    author: "FRANK HERBERT",
+    img: img1,
+    id: 1,
+  },
+  {
+    title: "The Women",
+    author: "KRISTIN HANNAH",
+    img: img2,
+    id: 2,
+  },
+  {
+    title: "Fourth Wing: Empyrean, Book 1",
+    author: "REBECCA YARROS",
+    img: img3,
+    id: 3,
+  },
+];
